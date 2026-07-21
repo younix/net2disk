edd3d290f165d2318c14eea96234c175ba765442
Jan Klemkow <j.klemkow@wemelug.de>
2026-07-21 12:18:37 +0200

    add getopt(3) and usage()

--- net2disk.c
+++ net2disk.c
@@ -1,5 +1,28 @@
-int
-main(void)
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void
+usage(void)
 {
+	fputs("net2disk [-h]\n", stderr);
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int ch;
+
+	while ((ch = getopt(argc, argv, "h")) != -1) {
+		switch (ch) {
+		case 'h':
+		default:
+			usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
 	return 0;
 }
